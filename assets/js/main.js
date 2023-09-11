fda7f433495e4a75bb16221069dc70333994e25c
davide-rullo <davide.rullo01@gmail.com>
2023-09-11 14:28:40 +0200

    add
    added script and html content

---
+++ assets/js/main.js
@@ -0,0 +1,12 @@
+const { createApp } = Vue
+createApp({
+    data() {
+      return {
+        message: 'Ciao!',
+        image: 'https://picsum.photos/800/600'
+      }
+      
+    }
+  }).mount('#app')
+
+ 
